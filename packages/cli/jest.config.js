909cfb5f2145b5aa6a9886878681557166d760ad
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-07-06 23:54:42 +0200

    refactor: Consolidate tsconfig and eslintrc files (no-changelog)

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -7,9 +7,5 @@ module.exports = {
 	globalSetup: '<rootDir>/test/setup.ts',
 	globalTeardown: '<rootDir>/test/teardown.ts',
 	setupFilesAfterEnv: ['<rootDir>/test/setup-mocks.ts', '<rootDir>/test/extend-expect.ts'],
-	moduleNameMapper: {
-		'^@/(.*)$': '<rootDir>/src/$1',
-		'^@db/(.*)$': '<rootDir>/src/databases/$1',
-	},
 	coveragePathIgnorePatterns: ['/src/databases/migrations/'],
 };
